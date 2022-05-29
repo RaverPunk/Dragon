678a9660ef93d2a32617b193e7f7e73661b656cc
raver <raverpunkz@gmail.com>
2022-05-29 12:34:41 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -249,7 +249,7 @@ function App() {
             >
               <StyledButton
                 onClick={(e) => {
-                  window.open("/config/roadmap.pdf", "_blank");
+                  window.open("/config/roadmap.html", "_blank");
                 }}
                 style={{
                   margin: "5px",
